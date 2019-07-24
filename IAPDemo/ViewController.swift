2137d086d145ef99b70405103411c36c519ca1ed
Talha <talhaayar53@gmail.com>
2019-07-24 15:19:26 +0300

    Initial Commit

---
+++ IAPDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAPDemo
+//
+//  Created by TALHA AYAR on 24.07.2019.
+//  Copyright © 2019 Talha Ayar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
