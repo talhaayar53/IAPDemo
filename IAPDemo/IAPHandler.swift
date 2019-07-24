3f0505678cf821c86afde801ea23008abe388e9a
Talha <talhaayar53@gmail.com>
2019-07-24 15:32:23 +0300

    IAPHandler file added

---
+++ IAPDemo/IAPHandler.swift
@@ -0,0 +1,121 @@
+//
+//  IAPHandler.swift
+//  IAPDemo
+//
+//  Created by TALHA AYAR on 24.07.2019.
+//  Copyright © 2019 Talha Ayar. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+enum IAPHandlerAlertType{
+    case disabled
+    case restored
+    case purchased
+    
+    func message() -> String{
+        switch self {
+        case .disabled: return "Purchases are disabled in your device!"
+        case .restored: return "You've successfully restored your purchase!"
+        case .purchased: return "You've successfully bought this purchase!"
+        }
+    }
+}
+
+
+class IAPHandler: NSObject {
+    static let shared = IAPHandler()
+    
+    
+    //YOU CAN ADD ANOTHER PRODUCT ID
+    fileprivate var productID = "YOUR IAP PRODUCT ID"
+    fileprivate var productsRequest = SKProductsRequest()
+    fileprivate var iapProducts = [SKProduct]()
+    
+    var purchaseStatusBlock: ((IAPHandlerAlertType) -> Void)?
+    
+    func canMakePurchases() -> Bool {  return SKPaymentQueue.canMakePayments()  }
+    
+    func purchaseMyProduct(index: Int){
+        if iapProducts.count == 0 { return }
+        
+        if self.canMakePurchases() {
+            let product = iapProducts[index]
+            let payment = SKPayment(product: product)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+            
+            print("PRODUCT TO PURCHASE: \(product.productIdentifier)")
+            productID = product.productIdentifier
+        } else {
+            purchaseStatusBlock?(.disabled)
+        }
+    }
+    
+    // MARK: - RESTORE PURCHASE
+    func restorePurchase(){
+        SKPaymentQueue.default().add(self)
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+    
+    
+    // MARK: - FETCH AVAILABLE IAP PRODUCTS
+    func fetchAvailableProducts(){
+        
+        // Put here your IAP Products ID's
+        let productIdentifiers = NSSet(objects: productID /*You can put here another product id */)
+        
+        productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers as! Set<String>)
+        productsRequest.delegate = self
+        productsRequest.start()
+    }
+}
+
+extension IAPHandler: SKProductsRequestDelegate, SKPaymentTransactionObserver{
+    // MARK: - REQUEST IAP PRODUCTS
+    func productsRequest (_ request:SKProductsRequest, didReceive response:SKProductsResponse) {
+        
+        if (response.products.count > 0) {
+            iapProducts = response.products
+            for product in iapProducts{
+                let numberFormatter = NumberFormatter()
+                numberFormatter.formatterBehavior = .behavior10_4
+                numberFormatter.numberStyle = .currency
+                numberFormatter.locale = product.priceLocale
+                let price1Str = numberFormatter.string(from: product.price)
+                print(product.localizedDescription + "\nfor just \(price1Str!)")
+            }
+        }
+    }
+    
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        purchaseStatusBlock?(.restored)
+    }
+    
+    // MARK:- IAP PAYMENT QUEUE
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction:AnyObject in transactions {
+            if let trans = transaction as? SKPaymentTransaction {
+                switch trans.transactionState {
+                case .purchased:
+                    print("purchased")
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    purchaseStatusBlock?(.purchased)
+                    UserDefaults.standard.set(true, forKey: "isPaid")
+                    break
+                    
+                case .failed:
+                    print("failed")
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+                case .restored:
+                    print("restored")
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    UserDefaults.standard.set(true, forKey: "isPaid")
+                    break
+                    
+                default: break
+                }}}
+    }
+}
--- IAPDemo/ViewController.swift
+++ IAPDemo/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    func IAP() {
+        IAPHandler.shared.restorePurchase()
+        IAPHandler.shared.purchaseMyProduct(index: 0)
     }
 
 
